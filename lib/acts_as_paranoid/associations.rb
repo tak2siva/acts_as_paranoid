521919559470a2d77cd41a9d97fd88733e3c04c6
tak2siva <tak2siva@outlook.com>
2013-11-15 17:54:26 +0530

    Update associations.rb
    :with_deleted option for has_many relationship (but not returning deleted result)

--- lib/acts_as_paranoid/associations.rb
+++ lib/acts_as_paranoid/associations.rb
@@ -4,6 +4,7 @@ module ActsAsParanoid
       base.extend ClassMethods
       class << base
         alias_method_chain :belongs_to, :deleted
+        alias_method_chain :has_many, :deleted
       end
     end
 
@@ -11,6 +12,28 @@ module ActsAsParanoid
       def belongs_to_with_deleted(target, options = {})
         with_deleted = options.delete(:with_deleted)
         result = belongs_to_without_deleted(target, options)
+
+        if with_deleted
+          result.options[:with_deleted] = with_deleted
+          unless method_defined? "#{target}_with_unscoped"
+            class_eval <<-RUBY, __FILE__, __LINE__
+              def #{target}_with_unscoped(*args)
+                association = association(:#{target})
+                return nil if association.options[:polymorphic] && association.klass.nil?
+                return #{target}_without_unscoped(*args) unless association.klass.paranoid?
+                association.klass.with_deleted.scoping { #{target}_without_unscoped(*args) }
+              end
+              alias_method_chain :#{target}, :unscoped
+            RUBY
+          end
+        end
+
+        result
+      end
+      
+      def has_many_with_deleted(target, options = {})
+        with_deleted = options.delete(:with_deleted)
+        result = has_many_without_deleted(target, options)
 
         if with_deleted
           result.options[:with_deleted] = with_deleted
